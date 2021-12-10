e0a7ff4ac6dc2129ede57b91669951136ff3b2b3
Martingit1110 <95227704+Martingit1110@users.noreply.github.com>
2021-12-10 14:43:41 -0300

    Add files via upload
    Ivan fijate que te ayude ahi a terminar de armar el menu y junte lo tuyo con lo mio. A mi me falta terminar la funcion 4 y 5 pero ya esta todo organizado. Te deje para que completes tambien las funciones que te faltan a vos

--- TP2_algoritmo.py
+++ TP2_algoritmo.py
@@ -1,17 +1,8 @@
-
-# Datos a tener en cuenta:
-#   para los pedidos, es conveniente una diccionario con el siguiente formato: {NdePedido, [fecha, cliente, ciudad, provincia, cod. articulo, color, cantidad, descuento,entregado]}
-#   Realizar algun tipo de checkeo para saber que pedidos fueron entregados y cuales no, tambien saber el motivo.
-#   Teniendo el punto anterior en cuenta, lo mejor seria dividir los pedidos en entregados y no entregados, de esta forma las funciones directamente agarran la lista/dict correspondiente sin tener que checkear repetidas veces que si se entrego cada pedido.
-
-# Dudas:
-#   Como checkear si un pedido es realizado o no, teniendo en cuenta que pedidos.csv no lo marca. Charlar con el grupo.
-#
+#Ivan
 
 import cv2
 import os
 import numpy as np
-
 
 def ABM(dir: str) -> str:
     lineas_nuevo_archivo: list = []
@@ -104,256 +95,156 @@ def ABM(dir: str) -> str:
 
     return dir
 
+#Martín
 
-def checkeo_pedidos_entregados(
-        listado_pedidos: dict) -> dict:  # Solución temporal hasta saber como checkear si un pedido fue entregado
-    pedidos_entregados: dict = listado_pedidos.copy()  # copio el dict original con TODOS los pedidos
-    for i in listado_pedidos:  # recorro dict
-        if listado_pedidos[i[
-            8]] == "no":  # si el pedido no fue entregado, lo borro del dict nuevo que hice. Por lo tanto, te quedan los pedidos que fueron entregados.
-            pedidos_entregados.pop(i)
-    return pedidos_entregados
+def crear_lista_pedidos_entregados(listado_pedidos: list) -> list:
+    lista_entregados: list = [] #lista vacia
+    for sublista in listado_pedidos: #recorro sublista 
+        if sublista[9] == "si":
+            lista_entregados.append(sublista)
+    return lista_entregados
+
+def crear_lista_pedidos_no_entregados(listado_pedidos: list) -> list:
+    lista_no_entregados: list = [] #lista vacia
+    for sublista in listado_pedidos: #recorro sublista 
+        if sublista[9] == "no":
+            lista_no_entregados.append(sublista)
+    return lista_no_entregados
+
+def pedidos_realizados(lista_pedidos_entregados: list) -> None:
+    #Pre: recibe todos los pedidos realizados (todavia no se en que formato, es probable que en dict)"
+    #Post: Printea los pedidos realizados, en orden de antiguedad y devuelve la cantidad de pedidos realizados en numero. 
+    numero_pedidos_entregados: int = 0 #Cantidad de pedidos inicializado
+    lista_ordenada: list = []
+    fecha_antigüedad: list = [32,13,3000] #día, mes, año
+    for sublista in lista_pedidos_entregados: #recorre lista
+        if sublista[1][2] < fecha_antigüedad[2]:
+           fecha_antigüedad[2] = sublista[1][2]
+        elif sublista[1][2] == fecha_antigüedad[2]:
+            if sublista[1][1] < fecha_antigüedad[1]:
+                fecha_antigüedad[1] = sublista[1][1]
+        elif sublista[1][2] == fecha_antigüedad[2]:
+            if sublista[1][1] == fecha_antigüedad[1]:
+                if sublista[1][0] < fecha_antigüedad[0]:
+                    fecha_antigüedad[0] = sublista[1][0]
+    print(fecha_antigüedad)
 
 
-def checkeo_pedidos_no_entregados(listado_pedidos: dict) -> dict:
-    pedidos_no_entregados: dict = listado_pedidos.copy()  # copio dict original con TODOS los pedidos
-    for i in listado_pedidos:  # recorro dict
-        if listado_pedidos[i[
-            8]] == "si":  # si el pedido fue entregado, lo borro del dict nuevo que hice. Por lo tanto, te quedan los pedidos que NO fueron entregados.
-            pedidos_no_entregados.pop(i)
-    return pedidos_no_entregados
 
 
-def pedidos_realizados(pedidos_entregados: dict) -> None:
-    # Pre: recibe todos los pedidos realizados (todavia no se en que formato, es probable que en dict)"
-    # Post: Printea los pedidos realizados, en orden de antiguedad y devuelve la cantidad de pedidos realizados en numero.
-    numero_pedidos_entregados: int = 0  # Cantiad de pedidos inicializado
-    for i in pedidos_entregados:  # recorre lista
-        numero_pedidos_entregados += 1  # Cuenta los pedidos realizados
-    sorted(pedidos_entregados, key=lambda fecha: pedidos_entregados[0])  # checkear este sorted
+        #numero_pedidos_entregados += 1 #Cuenta los pedidos realizados 
+        
+    #print(lista_pedidos_entregados) # printea la lista por orden de antigüedad.
+    print(f"La cantidad de pedidos realizados es: {numero_pedidos_entregados}") #printea la cantidad de pedidos realizados.
 
-    print()  # printea la lista por orden de antigüedad.
-    print(numero_pedidos_entregados)  # printea la cantidad de pedidos realizados.
+def pedidos_rosario(lista_pedidos_entregados: list) -> None:
+    #Pre: recibir los pedidos completados
+    #Post: filtrea los que tienen como ciudad rosario, los muestra y los valoriza
+    pedidos_rosario : list = [] #lista vacia
+    print(len(lista_pedidos_entregados))
+    for i in range(len(lista_pedidos_entregados)): #recorro dicc
+        if lista_pedidos_entregados[i][3] == "Rosario": #si la ciudad es Rosario
+            pedidos_rosario.append(lista_pedidos_entregados[i]) # Lo agrega a la lista vacia
+    print(pedidos_rosario)
     return
 
 
-def pedidos_rosario():
-    # Pre: recibir los pedidos
-    # Post: filtrea los que tienen como ciudad rosario, los muestra y los valoriza
+def cantidad_de_productos_pedidos(listado_pedidos: list, productos: dict) -> dict:
+    #Pre: recibe los pedidos
+    #Post: cuenta cada pedido en una lista, mostrando la cantidad de cada producto vendido
+    cantidad_botellas: dict = {"verde": 0, "rojo": 0, "azul": 0, "negro": 0, "amarillo": 0}
+    cantidad_vasos: dict = {"negro": 0, "azul": 0,}
+    for i in range(len(listado_pedidos)):
+        if listado_pedidos[i][5] == 1334:
+            if listado_pedidos[i][6] in cantidad_botellas.keys():
+                cantidad_botellas[listado_pedidos[i][6]] += listado_pedidos[i][7]
+        elif listado_pedidos[i][5] == 568:
+            if listado_pedidos[i][6] in cantidad_vasos.keys():
+                cantidad_vasos[listado_pedidos[i][6]] += listado_pedidos[i][7]
+    productos.update({"botella": cantidad_botellas})
+    productos.update({"vaso" : cantidad_vasos})
+    return productos
 
-    return
+def cantidad_de_productos_entregados(lista_pedidos_entregados: list, productos_entregados: dict) -> dict:
+    cantidad_botellas_entregadas: dict = {"verde": 0, "rojo": 0, "azul": 0, "negro": 0, "amarillo": 0}
+    cantidad_vasos_entregados: dict = {"negro": 0, "azul": 0,}
+    for i in range(len(lista_pedidos_entregados)):
+        if lista_pedidos_entregados[i][5] == 1334:
+            if lista_pedidos_entregados[i][6] in cantidad_botellas_entregadas.keys():
+                cantidad_botellas_entregadas[lista_pedidos_entregados[i][6]] += lista_pedidos_entregados[i][7]
+        elif lista_pedidos_entregados[i][5] == 568:
+            if lista_pedidos_entregados[i][6] in cantidad_vasos_entregados.keys():
+                cantidad_vasos_entregados[lista_pedidos_entregados[i][6]] += lista_pedidos_entregados[i][7]
+    productos_entregados.update({"botella": cantidad_botellas_entregadas})
+    productos_entregados.update({"vaso" : cantidad_vasos_entregados})
+    return productos_entregados
 
+def articulo_mas_pedido(productos: dict, productos_entregados: dict) -> None:
+    #Pre: Recibe el diccionario de productos y productos entregados.
+    #Post: muestra cual es el articulo mas pedido y cuantos fueron entregados.
+    producto_mas_pedido: str = "-" #inicializo variables
+    color: str = ""
+    cantidad_producto_mas_pedido: int = 0
+    if productos["botella"]["verde"] > cantidad_producto_mas_pedido: #si tiene la mayor cantidad de pedidos
+        cantidad_producto_mas_pedido = productos["botella"]["verde"] #lo guardo en esta variable
+        producto_mas_pedido = "botella"
+        color = "verde"
+    elif productos["botella"]["rojo"] > cantidad_producto_mas_pedido: #repito proceso con cad producto
+        cantidad_producto_mas_pedido = productos["botella"]["rojo"]
+        producto_mas_pedido = "botella"
+        color = "rojo"
+    elif productos["botella"]["azul"] > cantidad_producto_mas_pedido:
+        cantidad_producto_mas_pedido = productos["botella"]["azul"]
+        producto_mas_pedido = "botella"
+        color = "azul"
+    elif productos["botella"]["negro"] > cantidad_producto_mas_pedido:
+        cantidad_producto_mas_pedido = productos["botella"]["negro"]
+        producto_mas_pedido = "botella"
+        color = "negro"
+    elif productos["botella"]["amarillo"] > cantidad_producto_mas_pedido:
+        cantidad_producto_mas_pedido = productos["botella"]["amarillo"]
+        producto_mas_pedido = "botella"
+        color = "amarillo"
+    elif productos["vaso"]["negro"] > cantidad_producto_mas_pedido:
+        cantidad_producto_mas_pedido = productos["vaso"]["negro"]
+        producto_mas_pedido = "vaso"
+        color = "negro"
+    elif productos["vaso"]["azul"] > cantidad_producto_mas_pedido:
+        cantidad_producto_mas_pedido = productos["vaso"]["azul"]
+        producto_mas_pedido = "vaso"
+        color = "azul"
+    print(f"El producto más pedido es: {producto_mas_pedido} de color {color} con {cantidad_producto_mas_pedido} unidades.\n De esa cantidad, {productos_entregados[producto_mas_pedido][color]} fueron entregados.")
 
-def cantidad_de_productos_pedidos():
-    # Pre: recibe los pedidos
-    # Post: cuenta cada pedido en una lista, mostrando la cantidad de cada producto vendido
-    return
-
-
-def articulo_mas_pedido():
-    # Pre: Recibe la lista de cantidad_de_productos_pedidos y deberia recibir otra informacion mas que checkee cuantos fueron entregados.
-    # Post: muestra cual es el articulo mas pedido y cuantos fueron entregados.
-    return
-
-
-def deteccion_colores(imagen):
-    # Pre: la imagen tiene que poder mutar su color al rango hsv.
-    # Post: recibe una imagen y verifica si hay alguno de los 5 colores preestablecidos.
-    hsv = cv2.cvtColor(imagen, cv2.COLOR_BGR2HSV)
-    colores_rango = {
-        "verde": ([40, 100, 100], [75, 255, 255]),
-        "negro": ([0, 0, 0], [0, 0, 10]),
-        "rojo": ([161, 155, 84], [179, 255, 255]),
-        "azul": ([94, 80, 2], [126, 255, 255]),
-        "amarillo": ([25, 100, 100], [30, 255, 255])}
-    for nombre_color, (lower, upper) in colores_rango.items():
-        lower = np.array(lower, dtype=np.uint8)
-        upper = np.array(upper, dtype=np.uint8)
-        mask = cv2.inRange(hsv, lower, upper)
-        resultado = cv2.bitwise_and(imagen, imagen, mask=mask)
-        if mask.any():
-            print(nombre_color)
-
-
-def imagenes_carpeta(carpeta):
-    # Pre: tiene que ingresar una direccion que lleve a una carpeta con archivos dentro.
-    # Post: recorre y carga los archivos dentro de la carpeta recibida uno por uno.
-    for filename in os.listdir(carpeta):
-        img = cv2.imread(os.path.join(carpeta, filename))
-        deteccion_colores(img)
-        cv2.imshow("escaner", img)
-        cv2.waitKey(0)
-
-
-#Genesis---Deteccion Imagen 
-import cv2
-import numpy as np
-import os
-
-
-def load_yolo():
-    # Pos: levanta la red Neuronal con los archivos de pesos pre-entrenados de YoloV3, el archivo de configuración y el archivo de nombres
-
-    net = cv2.dnn.readNet("yolov3.weights", "yolov3.cfg")
-    classes: list = []
-    with open("coco.names", "r") as f:
-        classes = [line.strip() for line in f.readlines()]
-    layers_names = net.getLayerNames()
-    output_layers = [layers_names[i - 1] for i in net.getUnconnectedOutLayers()]
-
-    colors = np.random.uniform(0, 255, size=(len(classes), 3))
-    return net, classes, colors, output_layers
-
-
-def load_image(img_path):
-    # Pre: Debe recibir la ruta de una imegen
-    # Pos: lee la imagen  y cambia su tamaño  y retorna la imagen con las nuevas dimenciones ( alto, ancho..)
-
-    img = cv2.imread(img_path)
-    img = cv2.resize(img, None, fx=0.4, fy=0.4)
-    height, width, channels = img.shape
-
-    return img, height, width, channels
-
-
-def detect_objects(img, net, outputLayers):
-    # Pre: Recibe una imagen, y las capas de salida de la red
-    # Pos: Se aplica la  red neuronal sobre la imagen
-
-    blob = cv2.dnn.blobFromImage(img, scalefactor=0.00392, size=(320, 320), mean=(0, 0, 0), swapRB=True, crop=False)
-    net.setInput(blob)
-    outputs = net.forward(
-        outputLayers)  # devuelve una lista anidada con toda la info del onj detectado, ancho, alto etc
-    return blob, outputs
-
-
-def get_box_dimensions(outputs, height, width):
-    # Pre:
-    # Pos:
-
-    boxes = []
-    confs = []
-    class_ids = []
-    for output in outputs:
-        for detect in output:
-            scores = detect[5:]
-            # print(scores)
-            class_id = np.argmax(scores)
-            conf = scores[class_id]
-            if conf > 0.3:
-                center_x = int(detect[0] * width)
-                center_y = int(detect[1] * height)
-                w = int(detect[2] * width)
-                h = int(detect[3] * height)
-                x = int(center_x - w / 2)
-                y = int(center_y - h / 2)
-                boxes.append([x, y, w, h])
-                confs.append(float(conf))
-                class_ids.append(class_id)
-    return boxes, confs, class_ids
-
-
-def draw_labels(boxes, confs, colors, class_ids, classes, img):
-    # Pre:
-    # Pos:
-
-    indexes = cv2.dnn.NMSBoxes(boxes, confs, 0.5, 0.4)
-    for i in range(len(boxes)):
-        if i in indexes:
-            x, y, w, h = boxes[i]
-            label = str(classes[class_ids[i]])
-
-    return label
-
-
-def image_detect(img_path):
-    # Pre: Recibe la ruta de una imagen
-    # Pos: Invoca a distintas funciones para retonar la imagen detectada
-
-    model, classes, colors, output_layers = load_yolo()
-    image, height, width, channels = load_image(img_path)
-    blob, outputs = detect_objects(image, model, output_layers)
-    boxes, confs, class_ids = get_box_dimensions(outputs, height, width)
-    return draw_labels(boxes, confs, colors, class_ids, classes, image)
-
-
-def images_folder(folder):
-    # Pre: Recibe una carpeta con varios archivos
-    # Pos: Recorre todos los archivos que estan en la carpeta e invocan las funciones  image_detect y deteccion_colores pasado como parametro la ruta
-
-    for filename in os.listdir(folder):
-        image_path = os.path.join(folder, filename)
-        imagen_detectada = image_detect(image_path)
-
-        if imagen_detectada == 'cup' or imagen_detectada == 'bottle':
-            print(imagen_detectada)
-        else:
-            print("PROCESO DETENIDO, se reanuda en 1 minuto")
-
-        while imagen_detectada == 'cup' or imagen_detectada == 'bottle':
-            #deteccion_colores(image_path) # esto retorna el color
-
-  
-
-def contador_objetos( objeto_detectada: str, color: str):
+def volver_menu():
+    input('Presione ENTER para volver al menu: ')
     
-    #Pre: Recibe como parametro el objeto detectado y el color 
-    #Pos: Crea un contador/ Diccionario por objeto, teniendo como clave el color y valor la cantidad 
-    
-    contador_vasos: dict = {}
-    contador_termos: dict = {}
-
-    if objeto_detectada == "vasos":
-        if color in contador_vasos.values():
-            contador_vasos[color] += 1
-
-        else:
-            contador_vasos[color] = 1
-
-    else:
-        if color in contador_termos.values():
-            contador_vasos[color] += 1
-
-        else:
-            contador_termos[color] = 1
-
- 
-
-def crear_archivo_txt (diccionario_contador: dict, mensaje: str):
-    # Pre:
-    # Pos: crear un archivo.txt donde se guardara la cantidad de botellas o vasos procesadas por color. 
-
-    archivo = open(mensaje, 'w')
-    for clave, valor in diccionario_contador.items():
-        archivo.write(" {} :{}".format(clave, valor) + "\n")
-    archivo.close()
-
-
-
 def main():
-    listado_pedidos: dict = {1: [1 / 11 / 2021, "Juan Alvarez", "Villa María", "Córdoba", 1334, "Azul", 36, 5, "si"],
-                             # {NdePedido: [fecha, cliente, ciudad, provincia, cod. articulo, color, cantidad, descuento, entregado]
-                             2: [2 / 11 / 2021, "Carlos Rodriguez", "Parana", "Santa Fe", 1334, "Negro", 5, 0, "no"]}
 
-    pedidos_entregados: dict = checkeo_pedidos_entregados(listado_pedidos)
-    pedidos_no_entregados: dict = checkeo_pedidos_no_entregados(listado_pedidos)
-    print(pedidos_entregados)
-    print(pedidos_no_entregados)
-    return
+    #Martín
 
-    # Ivan---------------
-    # Variables para ABM
+    listado_pedidos: list = [[1,[1,11,2021],"Juan Alvarez", "Rosario", "Córdoba", 1334, "azul", 36, 5, "si"],#[NdePedido, fecha, cliente, ciudad, provincia, cod. articulo, color, cantidad, descuento, entregado]
+    [2, [2,11,2021], "Carlos Rodriguez", "Parana", "Santa Fe", 1334, "azul", 5, 0, "no"], [3, [2,11,2021], "Carlos Rodriguez", "Rosario", "Santa Fe", 1334, "negro", 5, 0, "no"]]
+    productos: dict = {}
+    productos_entregados: dict = {}
+    lista_pedidos_entregados: list = crear_lista_pedidos_entregados(listado_pedidos)
+    lista_pedidos_no_entregados: list = crear_lista_pedidos_no_entregados(listado_pedidos)
+    
+
+
+    #Ivan
     pedir_ruta_abm: int = 0
     dir_abm: str = ''
-    inicio_menu: int = 0
+    cerrar_menu: bool = False
 
-    while inicio_menu == 0:
+    #Menú --> Ivan = 1, 2, 3
+    #Martín = 4, 5, 6
 
+    while cerrar_menu == False:
         print('Bienvenido al MENU')
-        print('1: Alta - Baja - Modificacion de pedidos')
+        print('1: Alta - Baja - Modificacion de pedidos\n4:Listado de pedidos que fueron completados.\5Pedidos de Rosario con su valorización\n6.Artículo más pedido y cuantos fueron entregados.\n7.Salir')
 
         accion = input('Elija opcion escribiendo el numero correspondiente: ')
-        while accion.isnumeric() is False or int(accion) not in (1, 2, 3, 4):
+        while accion.isnumeric() is False or int(accion) not in (1, 2, 3, 4, 5, 6, 7):
             accion: str = input('Ingrese una opcion valida: ')
 
         if int(accion) == 1:
@@ -367,7 +258,27 @@ def main():
                 pedir_ruta_abm = 1
 
             dir_abm = ABM(dir_abm)
-            volver_menu = input('Presione ENTER para volver al menu: ')
+            volver_menu()
+        elif int(accion) == 2:
+            return #ivan
+        elif int(accion) == 3:
+            return # ivan
+        elif int(accion) == 4:
+            pedidos_realizados(lista_pedidos_entregados)
+        elif int(accion) == 5:
+            pedidos_rosario(lista_pedidos_entregados)
+        elif int(accion) == 6:
+            cantidad_de_productos_pedidos(listado_pedidos, productos)
+            cantidad_de_productos_entregados(lista_pedidos_entregados, productos_entregados)
+            articulo_mas_pedido(productos, productos_entregados)
+            volver_menu()
+        elif int(accion) == 7:
+            print("Abandonando el menú...")
+            cerrar_menu = True 
+    
+  
+   
+    
 
 
 main()
